0978ee1963dbd0876ba235c4b17e9c46df434d48
liqiang <liqiang@buckyos.com>
2022-08-12 17:32:49 +0800

    add some new feature to play

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,47 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use std::env;
 use tauri::Manager;
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
 #[tauri::command]
 fn hello_test(word: String) -> String {
     format!("Hello, {}", word)
 }
+
+#[tauri::command]
+fn show_main_window(window: tauri::Window) {
+    if let Some(window) = window.get_window("splashscreen") {
+        window.close().unwrap();
+    }
+    window.get_window("main").unwrap().show().unwrap();
+}
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+    let quit = CustomMenuItem::new("quit".to_string(), "QUIT");
+    let hide = CustomMenuItem::new("hide".to_string(), "HIDE");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+    let system_tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
+        .system_tray(system_tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                "quit" => {
+                    let window = app.get_window("main").unwrap();
+                    window.close().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .setup(|app| {
             app.listen_global("backend", |event| {
                 println!(
@@ -19,7 +52,7 @@ fn main() {
             });
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![hello_test])
+        .invoke_handler(tauri::generate_handler![hello_test, show_main_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import styles from "./App.module.less";
 import { invoke } from "@tauri-apps/api";
 import { emit } from "@tauri-apps/api/event";
+import { WebviewWindow } from "@tauri-apps/api/window";
 
 function App() {
+	useEffect(() => {
+		setTimeout(() => {
+			invoke("show_main_window");
+		}, 2000);
+	}, []);
 	const handleClick = async () => {
 		const ret = await invoke("hello_test", { word: "TRURI" });
 		alert(ret);
@@ -12,10 +18,23 @@ function App() {
 	const handleBackend = () => {
 		emit("backend", { word: "FrontEnd" });
 	};
+
+	const handleOpenNewWindow = () => {
+		const webview = new WebviewWindow("new-window", {
+			url: "https://github.com/tauri-apps/tauri",
+		});
+		webview.once("tauri://created", function () {
+			// webview window successfully created
+		});
+		webview.once("tauri://error", function (e) {
+			// an error happened creating the webview window
+		});
+	};
 	return (
 		<div className={styles.box}>
 			<h1 onClick={handleClick}> Hello Tauri!</h1>
 			<button onClick={handleBackend}>call backend</button>
+			<button onClick={handleOpenNewWindow}>open new window</button>
 		</div>
 	);
 }
